81f7fdee90521cc2d3be171f289ea08a565c058a
Michal <michalgrzegorczyk04@gmail.com>
2024-03-06 14:11:23 +0100

    modyfication of change head

--- game/game_state.py
+++ game/game_state.py
@@ -11,36 +11,15 @@ class GameState:
     def next_head(self, direction):
         pos = self.snake[-1]
         if direction == Direction.UP:
-            return Position(pos.x, pos.y - 1)
+            return Position(pos.x, (pos.y - 1) % self.field_size)
         elif direction == Direction.DOWN:
-            return Position(pos.x, pos.y + 1)
+            return Position(pos.x, (pos.y + 1) % self.field_size)
         elif direction == Direction.RIGHT:
-            return Position(pos.x + 1, pos.y)
+            return Position((pos.x + 1) % self.field_size, pos.y)
         elif direction == Direction.LEFT:
-            return Position(pos.x - 1, pos.y)
+            return Position((pos.x - 1) % self.field_size, pos.y)
 
     def step(self):
         new_head = self.next_head(self.direction)
         self.snake.append(new_head)
         self.snake = self.snake[1:]
-
-# state = GameState(
-# 	snake = [
-# 		Position(4, 0),
-# 		Position(4, 1),
-# 		Position(4, 2)
-# 	],
-# 	direction = Direction.DOWN,
-# 	food = Position(10, 10),
-# 	field_size = 20
-# )
-
-# state.step()
-
-# expected_state = [
-# 	Position(4, 1),
-# 	Position(4, 2),
-# 	Position(4, 3)
-# ]
-
-# self.assertEqual(expected_state, state.snake)
