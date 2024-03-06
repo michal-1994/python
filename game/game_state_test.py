08f332e911373dc91c6f6f6bdd9db9c00b4b761b
Michal <michalgrzegorczyk04@gmail.com>
2024-03-06 14:07:59 +0100

    add tests for edge of map

--- game/game_state_test.py
+++ game/game_state_test.py
@@ -18,13 +18,11 @@ class GameStateTest(unittest.TestCase):
 		)
 
         state.step()
-
         expected_state = [
 			Position(1, 3),
 			Position(1, 4),
 			Position(2, 4)
 		]
-
         self.assertEqual(expected_state, state.snake)
 
     def test_snake_should_move_left(self):
@@ -40,13 +38,11 @@ class GameStateTest(unittest.TestCase):
 		)
 
         state.step()
-
         expected_state = [
 			Position(1, 3),
 			Position(1, 4),
 			Position(0, 4)
 		]
-
         self.assertEqual(expected_state, state.snake)
 
     def test_snake_should_move_up(self):
@@ -62,13 +58,11 @@ class GameStateTest(unittest.TestCase):
 		)
 
         state.step()
-
         expected_state = [
 			Position(1, 1),
 			Position(1, 2),
 			Position(1, 3)
 		]
-
         self.assertEqual(expected_state, state.snake)
 
     def test_snake_should_move_down(self):
@@ -84,13 +78,51 @@ class GameStateTest(unittest.TestCase):
 		)
 
         state.step()
-
         expected_state = [
 			Position(1, 3),
 			Position(1, 4),
 			Position(1, 5)
 		]
+        self.assertEqual(expected_state, state.snake)
 
+    def test_snake_should_move_up_on_top(self):
+        state = GameState(
+			snake = [
+				Position(2, 2),
+				Position(2, 1),
+				Position(2, 0)
+			],
+			direction = Direction.UP,
+   			food = Position(10, 10),
+			field_size = 20
+		)
+
+        state.step()
+        expected_state = [
+			Position(2, 1),
+			Position(2, 0),
+			Position(2, 19)
+		]
+        self.assertEqual(expected_state, state.snake)
+
+    def test_snake_should_move_right_on_edge(self):
+        state = GameState(
+			snake = [
+				Position(17, 1),
+				Position(18, 1),
+				Position(19, 1)
+			],
+			direction = Direction.RIGHT,
+   			food = Position(10, 10),
+			field_size = 20
+		)
+
+        state.step()
+        expected_state = [
+			Position(18, 1),
+			Position(19, 1),
+			Position(0, 1)
+		]
         self.assertEqual(expected_state, state.snake)
 
 if __name__ == '__main__':
