91eefa8ba57634d78c871ccc964323351791cde5
Michal <michalgrzegorczyk04@gmail.com>
2024-03-06 09:43:38 +0100

    draw parts of snake

--- game.py
+++ game.py
@@ -18,6 +18,18 @@ screen.fill(WHITE)
 
 pygame.display.update()
 
+def draw_snake_part(x, y):
+    position = (x * CUBE_SIZE, y * CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
+
+    pygame.draw.rect(screen, GREEN, position)
+    pygame.display.update()
+
+draw_snake_part(0, 0)
+draw_snake_part(13, 16)
+draw_snake_part(10, 10)
+draw_snake_part(12, 8)
+draw_snake_part(18, 18)
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
