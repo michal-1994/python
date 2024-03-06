a6e82774328c562405d8b96922ce9a94d1f194ff
Michal <michalgrzegorczyk04@gmail.com>
2024-03-06 13:32:13 +0100

    code refactor

--- game/game.py
+++ game/game.py
@@ -8,15 +8,6 @@ CUBE_SIZE = 25
 CUBE_NUM = 20
 WIDTH = CUBE_SIZE * CUBE_NUM
 
-# COLORS
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
-
-# SETTING GAME
-screen = pygame.display.set_mode((WIDTH, WIDTH))
-screen.fill(WHITE)
-
 snake = [
 	Position(2, 2),
 	Position(3, 2),
@@ -27,27 +18,38 @@ snake = [
 
 food = Position(11, 14)
 
-pygame.display.update()
+# COLORS
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+# SETTING GAME
+screen = pygame.display.set_mode((WIDTH, WIDTH))
+screen.fill(WHITE)
 
 def draw_snake_part(pos):
 	part_position = (pos.x * CUBE_SIZE, pos.y * CUBE_SIZE, CUBE_SIZE, CUBE_SIZE)
-
 	pygame.draw.rect(screen, GREEN, part_position)
-	pygame.display.update()
-
-def draw_food(pos):
-	radius = float(CUBE_SIZE) / 2
-	food_position = (pos.x * CUBE_SIZE + radius, pos.y * CUBE_SIZE + radius)
-
-	pygame.draw.circle(screen, GREEN, food_position, radius)
-	pygame.display.update()
 
 def draw_snake(snake):
     for part in snake:
         draw_snake_part(part)
 
-draw_snake(snake)
-draw_food(food)
+def draw_food(pos):
+	radius = float(CUBE_SIZE) / 2
+	food_position = (pos.x * CUBE_SIZE + radius, pos.y * CUBE_SIZE + radius)
+	pygame.draw.circle(screen, BLUE, food_position, radius)
+
+def fill_bg():
+	screen.fill(WHITE)
+
+def draw(snake, food):
+    fill_bg()
+    draw_snake(snake)
+    draw_food(food)
+    pygame.display.update()
+
+draw(snake, food)
 
 while True:
 	for event in pygame.event.get():
