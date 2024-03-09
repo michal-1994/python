445c1aabf1eb4f25648ad9726ec9b51d3c7f08c7
Michal <michalgrzegorczyk04@gmail.com>
2024-03-09 14:17:20 +0100

    create titanic survival rate by class

---
+++ data_analysis/analysis.py
@@ -0,0 +1,27 @@
+import csv
+
+file_path = "C:/Users/Michal/OneDrive/Pulpit/Python/data_analysis/titanic.csv"
+file = open(file_path)
+data = [row for row in csv.reader(file)]
+file.close()
+header = data[0]
+data = data[1:]
+
+rows_num = len(data)
+
+class_1 = [row for row in data if row[1] == '1st']
+class_2 = [row for row in data if row[1] == '2nd']
+class_3 = [row for row in data if row[1] == '3rd']
+
+def print_survival_rate(data):
+    count = 0
+    survived = 0
+    for row in data:
+        count += 1
+        if row[4] == '1':
+            survived += 1
+    print(float(survived) / count)
+
+print_survival_rate(class_1)
+print_survival_rate(class_2)
+print_survival_rate(class_3)
